ed943f0abbaf270678104c480762647846c1a9a9
Shane Overby <soverby@coretechs.net>
2018-04-25 14:05:56 -0400

    initial

---
+++ FilterBranchTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FilterBranchTest.Startup))]
+namespace FilterBranchTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
